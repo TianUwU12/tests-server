3e1d79bdfc9a1bbf79b51dc550958f4c2867aa8e
TianUwU12 <n62760148@gmail.com>
2026-02-16 18:48:14 +0300

    set the cors

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const testRoutes = require("./routes/test-routes");
+const cors = require("cors");
 const app = express();
 const port = 3000;
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    optionsSuccessStatus: 200,
+  })
+);
 app.use("/api/tests", testRoutes);
+
 app.listen(port, () => {
   console.log("blablabla3");
 });
