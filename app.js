4ecb26f9e2fc81df2f26e13cffde4dcf24085511
Sergei Okhotnikov <sokhotnikoff@gmail.com>
2026-02-28 13:20:22 +0100

    Load env and add MongoDB connection

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const testRoutes = require("./routes/test-routes");
 const authRoutes = require("./routes/auth-routes");
-const cors = require("cors");
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB успешно подключена");
+  } catch (err) {
+    console.error("❌ Ошибка подключения к БД:", err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.use(
   cors({
@@ -14,20 +29,10 @@ app.use(
   }),
 );
 app.use(express.json());
+
 app.use("/api/tests", testRoutes);
 app.use("/api", authRoutes);
 
-// + endpoint
-// register
-// auth
-// checkTest + mark
-
-// + DB
-
-// + Model (user,test,category)
-
-//+middleware auth
-
 app.listen(port, () => {
-  console.log("blablabla3");
+  console.log(`🚀 Сервер запущен на http://localhost:${port}`);
 });
